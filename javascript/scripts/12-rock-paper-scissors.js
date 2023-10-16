641915dd46fdc8d5701c655f35018240569b83ac
sabasyedcodes <sabaa9018@gmail.com>
2023-10-16 11:19:48 +0530

    shortcut to write functions , event listener, foreach

--- javascript/scripts/12-rock-paper-scissors.js
+++ javascript/scripts/12-rock-paper-scissors.js
@@ -8,9 +8,10 @@ let score = JSON.parse(localStorage.getItem('score')) ||{
 
       let isAutoPlaying = false;
       let intervalId;
+      //const autoPlay = ()=>{};
       function autoPlay(){
         if (!isAutoPlaying){
-          intervalId = setInterval(function(){
+          intervalId = setInterval(()=>{
             const playerMove = pickComputerMove();
             playGame(playerMove);
           },1000);
@@ -20,9 +21,27 @@ let score = JSON.parse(localStorage.getItem('score')) ||{
           isAutoPlaying = false;
         }
       }
-          
-
       
+      document.querySelector('.js-rock-button').addEventListener('click',()=>{
+        playGame('rock');
+      });
+
+      document.querySelector('.js-paper-button').addEventListener('click',()=>{
+        playGame('paper');
+      });
+      document.querySelector('.js-scissors-button').addEventListener('click',()=>{
+        playGame('scissors');
+      });
+
+      document.body.addEventListener('keydown',(event)=>{
+        if(event.key ==='r'){
+          playGame('rock');
+        }else if (event.key === 'p'){
+          playGame('paper');
+        }else if(event.key === 's'){
+          playGame('scissors');
+        }
+      });
       function playGame(playerMove){
         const computerMove = pickComputerMove();
 
